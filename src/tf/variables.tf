857dff66286bc624ac363a3b5826fb1c62021b2e
asabahshah <99993954+asabahshah@users.noreply.github.com>
2024-01-02 16:44:39 +0000

    Update variables.tf

--- src/tf/variables.tf
+++ src/tf/variables.tf
@@ -9,12 +9,12 @@ variable "security_group_ids" {
 }
 
 variable "home_wifi_cidr" {
-  description = "CIDR notation for your home WiFi network"
-  default     = "192.168.0.0/24" #en0 INET FOR CIDR NOTATION
-  #default     = "192.168.184.108/32" OFFICE WIFI FOR TESTING SWAP BACK
+  description = ""
+  default     = "" 
+
 }
 
 variable "cidr_block" {
   description = "CIDR block address range"
-  default     = "10.0.0.0/16"
+  default     = ""
 }
